cc4c813e65d940501f4c926410b5586c4c14c627
John Hodge <johnhodge@MacBook-Pro.local>
2020-07-22 19:50:56 -0400

    Fixed errors in 'test2.py'.

--- test2.py
+++ test2.py
@@ -1,11 +1,13 @@
-while True: 
+x = 0
+
+while x == 0: 
     try: 
         fave_num = int(input("What's your favorite number? "))
-        return False
-    else: 
+        x += 1
+    except: 
         print("You must enter an integer...")
 
 if fave_num % 2 == 0: 
     print("Your favorite number is even!")
-else: 
+else:
     print("Your favorite number is odd!")
